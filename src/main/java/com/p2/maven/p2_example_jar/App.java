1ba462fa0081c5b123004b0e7219f96b94e03074
msk2143 <masa23aiml@cmrit.ac.in>
2026-06-17 20:45:38 +0530

    first commit

---
+++ src/main/java/com/p2/maven/p2_example_jar/App.java
@@ -0,0 +1,13 @@
+package com.p2.maven.p2_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to p2" );
+    }
+}
